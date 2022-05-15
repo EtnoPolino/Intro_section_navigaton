00b566a05ca76b92ac15b67d92ae5f04f26f0bb1
EtnoPolino <einshoch66@gmail.com>
2022-05-16 01:44:44 +0200

    second

---
+++ script.js
@@ -0,0 +1,43 @@
+const navChoices = document.querySelectorAll('#header-left ul li a');
+const arrowIcon = document.querySelector('.fa-angle-down');
+const subMenus = document.querySelectorAll('.sub-menu');
+
+const subMenu2 = document.querySelectorAll('#list-nav li');
+
+
+
+navChoices.forEach( (navChoice) => {
+    navChoice.addEventListener('click', test);
+});
+
+function test(e){
+
+    subMenu2.forEach(sub => {
+        console.log(sub.children[0]);
+    })
+    
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* subMenus.forEach((subMenu) => {
+        subMenu.classList.toggle('active');
+    }) */
+
+   /* if(this.children[0]){
+        this.children[0].classList.toggle('rotate');
+        subMenu.classList.toggle('active');
+    } */
+    
