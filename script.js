a5c6dce85dfdc7d5edffd32aebc3961d256abfdd
EtnoPolino <einshoch66@gmail.com>
2022-05-21 20:55:22 +0200

    desktop + mobile done

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
-/*
-const navChoices = document.querySelectorAll('#header-left ul li a');
-const arrowIcon = document.querySelector('.fa-angle-down');
-const subMenus = document.querySelectorAll('.sub-menu');
+const openNavMenu = document.querySelector("header #open");
+const navigationMenu = document.querySelector("header #header-left");
+const menuOverlay = document.querySelector("header #menu-overlay");
+const closeNavMenu = document.querySelector("header #close");
+const WINDOW_SIZE = 1000;
 
-const subMenu2 = document.querySelectorAll('#list-nav li');
+openNavMenu.addEventListener("click", toggleNavigation);
+closeNavMenu.addEventListener("click", toggleNavigation);
+navigationMenu.addEventListener("click", navigateOnMenu);
+menuOverlay.addEventListener("click", toggleNavigation);
 
-
-
-navChoices.forEach( (navChoice) => {
-    navChoice.addEventListener('click', test);
+/* ---we need to close our navigation bar and remove le black overlay when we expand--- */
+window.addEventListener("resize", () => {
+  if (window.innerWidth > WINDOW_SIZE) {
+    resizeFix();
+  }
 });
 
-function test(e){
+function toggleNavigation() {
+  navigationMenu.classList.toggle("open");
+  menuOverlay.classList.toggle("active");
+}
 
-    subMenu2.forEach(sub => {
-        console.log(sub.children[0]);
-    })
-    
+function navigateOnMenu(event) {
+  if (event.target.hasAttribute("data-toggle")) {
+    event.preventDefault();
+  }
 
-};
+  const listMenuHasChildren = event.target.parentElement;
+  const subMenu = listMenuHasChildren.querySelector(".sub-menu ul");
 
+  if (
+    event.target.hasAttribute("data-toggle") &&
+    window.innerWidth <= WINDOW_SIZE
+  ) {
+    if (listMenuHasChildren.classList.contains("list-nav-has-children")) {
+      if (subMenu.style.maxHeight) {
+        subMenu.style.maxHeight = null;
+      } else {
+        subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+      }
+    }
+  }
+}
 
-*/
+function resizeFix() {
+  /* closing the navigation menu and removing the overlay  */
+  if (navigationMenu.classList.contains("open")) {
+    toggleNavigation();
+  }
 
-
-
-
-
-
-
-
-
-
-
-   /* subMenus.forEach((subMenu) => {
-        subMenu.classList.toggle('active');
-    }) */
-
-   /* if(this.children[0]){
-        this.children[0].classList.toggle('rotate');
-        subMenu.classList.toggle('active');
-    } */
-    
+  /* closing the sub-menu inside of the navigation */
+  const listMenuHasChildrens = document.querySelectorAll(
+    "header #header-left .list-nav-has-children .sub-menu ul"
+  );
+  listMenuHasChildrens.forEach((child) => {
+    if ((child.style.maxHeight = child.scrollHeight + "px")) {
+      child.style.maxHeight = null;
+    }
+  });
+}
